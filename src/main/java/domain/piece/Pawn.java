7e5adaab059e26eb103ee5b498b477b76c70134b
eom-tae-in <eti0728@daum.net>
2023-06-24 22:45:27 +0900

    feat: Pawn 클래스 추가

---
+++ src/main/java/domain/piece/Pawn.java
@@ -0,0 +1,11 @@
+package domain.piece;
+
+import domain.Name;
+import domain.Team;
+
+public class Pawn extends Piece{
+
+    public Pawn(final Team team, final Name name) {
+        super(team, name);
+    }
+}
