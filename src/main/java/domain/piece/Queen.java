06180354024c2d388b0c48702791890565c314dd
eom-tae-in <eti0728@daum.net>
2023-06-24 22:45:41 +0900

    feat: Queen 클래스 추가

---
+++ src/main/java/domain/piece/Queen.java
@@ -0,0 +1,11 @@
+package domain.piece;
+
+import domain.Name;
+import domain.Team;
+
+public class Queen extends Piece{
+
+    public Queen(final Team team, final Name name) {
+        super(team, name);
+    }
+}
