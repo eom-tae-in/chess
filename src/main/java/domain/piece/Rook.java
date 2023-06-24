bd72ab33eb7a67df0a8f29072b959229a94ea5e1
eom-tae-in <eti0728@daum.net>
2023-06-24 22:45:45 +0900

    feat: Rook 클래스 추가

---
+++ src/main/java/domain/piece/Rook.java
@@ -0,0 +1,11 @@
+package domain.piece;
+
+import domain.Name;
+import domain.Team;
+
+public class Rook extends Piece {
+
+    public Rook(final Team team, final Name name) {
+        super(team, name);
+    }
+}
